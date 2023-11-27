9016ab9cca7a169a7fbbedc9ca58b0040d31b4fa
Thomas <viltetomas2003@gmail.com>
2023-11-27 13:15:50 -0300

    type inference

--- HolaMundo Java/src/HolaMundo.java
+++ HolaMundo Java/src/HolaMundo.java
@@ -1,7 +1,25 @@
 // Mi clase Java
 public class HolaMundo {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+
+        // Cambiar valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena =  "Adios";
+        System.out.println(miVariableCadena);
+
+        //var - Inferencia de tipos en Java
+        var miVariableEntera2 = 10;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena2 = "Nueva cadena";
+        System.out.println(miVariableCadena2);
     }
 }
+
