d90b614d62e4764be63832ebd073d2f4e171a981
Thomas <viltetomas2003@gmail.com>
2023-12-13 17:54:23 -0300

    ejercicio

--- HolaMundo Java/src/HolaMundo.java
+++ HolaMundo Java/src/HolaMundo.java
@@ -111,6 +111,27 @@ public class HolaMundo {
         } else {
             System.out.println("Invierno");
         }
+
+        var numero = 2;
+        var numeroTexto = "Valor desconocido";
+
+        switch(numero) {
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Caso desconocido";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
 
